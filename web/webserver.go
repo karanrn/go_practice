9f48ca54105743e90904d79539c6103af32fd4ed
Karan Nadagoudar <karan4080@gmail.com>
2020-09-12 11:03:23 +0530

    Added middleware for authentication

--- web/webserver.go
+++ web/webserver.go
@@ -2,13 +2,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
+// Implementing middleware for authentication
+func authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+		if len(authHeader) != 2 {
+			fmt.Println("Malformed token")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Malformed Token"))
+		} else {
+			jwtToken := authHeader[1]
+
+			token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+				return []byte("ASFF!@#KJSKAJD"), nil
+			})
+
+			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+				ctx := context.WithValue(r.Context(), "props", claims)
+				next.ServeHTTP(w, r.WithContext(ctx))
+			} else {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Unauthorized"))
+			}
+		}
+	})
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.Handle("/", authenticate(http.HandlerFunc(handler)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
